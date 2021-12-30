e9f496e9baff2d348ffb889e8316c4648f48cf97
Alfredo Otávio <alfredotavio@gmail.com>
2021-12-30 18:31:31 -0300

    Create install-zabbix-agent.bat

---
+++ install-zabbix-agent.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+
+REM Diretório raiz
+cd C:\
+
+REM Baixar instalador do Zabbix Agent
+curl https://raw.githubusercontent.com/alfredotavio/install-zabbix-windows/4d1b25e29bed2e8e418a3126a069734f4f824e11/zabbix_agent.msi --output zabbix_agent.msi
+
+REM Instalar Zabbix Agent
+msiexec /l*v install-zabbix-agent-log.txt /i zabbix_agent.msi^
+ SERVER=zabbix.grupomytec.com.br^
+ SERVERACTIVE=zabbix.grupomytec.com.br^
+ HOSTNAME=MYTEC_WIN_TESTE^
+ TIMEOUT=15^
+ /qn
