f2d1c0d6bc6c9feb32ccabc11db0e6a8e2e0ed37
Alfredo Otávio <alfredotavio@gmail.com>
2021-12-30 20:44:32 -0300

    Update install-zabbix-agent.bat

--- install-zabbix-agent.bat
+++ install-zabbix-agent.bat
@@ -4,7 +4,7 @@ REM Diretório raiz
 cd C:\
 
 REM Baixar instalador do Zabbix Agent
-curl https://raw.githubusercontent.com/alfredotavio/install-zabbix-windows/4d1b25e29bed2e8e418a3126a069734f4f824e11/zabbix_agent.msi --output zabbix_agent.msi
+curl https://raw.githubusercontent.com/alfredotavio/install-zabbix-windows/main/zabbix_agent.msi --output zabbix_agent.msi
 
 REM Instalar Zabbix Agent
 msiexec /l*v install-zabbix-agent-log.txt /i zabbix_agent.msi^
