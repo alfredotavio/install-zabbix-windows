93e52d6b8d7bc16a41f4f2c3cbc56d7934e14d5b
Alfredo Otávio <alfredotavio@gmail.com>
2023-10-30 22:48:33 -0300

    update

--- install-zabbix-agent.bat
+++ install-zabbix-agent.bat
@@ -4,14 +4,14 @@ REM Definindo argumentos
 set /p server=Digite o IP/DNS do Zabbix Server: 
 set /p hostname=Digite o Hostname do Zabbix Agent: 
 
-REM Diretório temporário
-cd %TEMP%
-
 REM Baixar instalador do Zabbix Agent
-curl https://cdn.zabbix.com/zabbix/binaries/stable/6.0/6.0.22/zabbix_agent-6.0.22-windows-amd64-openssl.msi --silent --output zabbix_agent.msi
+curl https://cdn.zabbix.com/zabbix/binaries/stable/6.0/6.0.22/zabbix_agent-6.0.22-windows-amd64-openssl.msi --silent --output %TEMP%\zabbix_agent.msi
+
+REM Liberar porta 10050 no Firewall
+netsh advfirewall firewall add rule name="Zabbix Agent" dir=in action=allow protocol=TCP localport=10050
 
 REM Instalar Zabbix Agent
-msiexec /l*v install-zabbix-agent-log.txt /i zabbix_agent.msi^
+msiexec /l*v %TEMP%\install-zabbix-agent-log.txt /i %TEMP%\zabbix_agent.msi^
  SERVER=%server%^
  SERVERACTIVE=%server%^
  HOSTNAME=%hostname%^
